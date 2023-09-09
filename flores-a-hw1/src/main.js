8060af80854d36f92cb8ba46dd64aa375add1d1c
BeastlyKoboi <itsallanflores06@gmail.com>
2023-09-08 20:42:26 -0400

    finished HW 1

---
+++ flores-a-hw1/src/main.js
@@ -0,0 +1,55 @@
+let words1
+let words2;
+let words3;
+
+let output;
+
+import { getRandomItem } from "./utils.js";
+
+const getTechnobabble = (lines = 1) => {
+    if (!output)
+        output = document.querySelector("#output");
+
+    output.innerHTML = ``;
+    for (let i = 0; i < lines; i++) {
+        output.innerHTML += `<p>${getRandomItem(words1)} ${getRandomItem(words2)} ${getRandomItem(words3)}</p>`;
+    }
+};
+
+const babbleLoaded = (e) => {
+    console.log(`In onload - HTTP Status Code = ${e.target.status}`);
+
+    const response = e.target.responseText;
+    if (!response) {
+        document.querySelector("#output").innerHTML = "Response is null!";
+    }
+
+    let jsonObject = JSON.parse(response);
+
+    words1 = jsonObject.words1;
+    words2 = jsonObject.words2;
+    words3 = jsonObject.words3;
+
+    const babbleBtn = document.querySelector("#babble-btn");
+    const multiBabbleBtn = document.querySelector("#multi-babble-btn");
+
+    babbleBtn.addEventListener("click", () => {
+        getTechnobabble(1);
+    });
+    multiBabbleBtn.addEventListener("click", () => {
+        getTechnobabble(5);
+    });
+
+    getTechnobabble(1);
+};
+
+const loadBabble = () => {
+    const url = "data/babble-data.json";
+    const xhr = new XMLHttpRequest();
+    xhr.onload = babbleLoaded;
+    xhr.onerror = e => console.log(`In onerror - HTTP Status Code = ${e.target.status}`);
+    xhr.open("GET", url);
+    xhr.send();
+};
+
+loadBabble();
